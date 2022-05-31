513055efc6e918f9199fa3b1ff0d6025159fd760
Braily Guzman <94770717+BGxo@users.noreply.github.com>
2022-05-30 22:33:23 -0400

    Update quiz.py
    just had to fix the input statements a little bit.

--- quiz.py
+++ quiz.py
@@ -2,8 +2,8 @@ import time
 
 print("Welcome To The Networking Quiz! \nYou are Able to research any answers you don't know, remember that we learn from our mistakes! :)")
 
-ready = input("Are you ready? Y/N: ")
-if ready == "Y" or "y":
+ready = input("Are you ready? Y/N: ").upper()
+if ready == "Y":
     print("Alright! Starting The 10 Questions Quiz!")
 else:
     quit()
@@ -13,8 +13,8 @@ else:
 def quiz():
     question_1 = "1) What does IP stands for?"
     print(question_1)
-    response_1 = input("Enter: ")
-    if (response_1) == "Internet Protocol" or "internet protocol":
+    response_1 = input("Enter: ").upper()
+    if (response_1) == "Internet Protocol":
         print("Correct!")
     else:
         print("Incorrect!")
@@ -23,8 +23,8 @@ def quiz():
 
     question_2 = "2) What does TCP stands for?"
     print(question_2)
-    response_2 = input("Enter: ")
-    if (response_2) == "Internet Protocol" or "internet protocol":
+    response_2 = input("Enter: ").upper()
+    if (response_2) == "Internet Protocol":
         print("Correct!")
     else:
         print("Incorrect!")
@@ -33,8 +33,8 @@ def quiz():
 
     question_3 = "3) What does UDP stands for?"
     print(question_3)
-    response_3 = input("Enter: ")
-    if (response_3) == "User Datagram Protocol" or "user datagram protocol":
+    response_3 = input("Enter: ").upper()
+    if (response_3) == "User Datagram Protocol":
         print("Correct!")
     else:
         print("Incorrect!")
@@ -43,8 +43,8 @@ def quiz():
 
     question_4 = "4) What does DNS stands for?"
     print(question_4)
-    response_4 = input("Enter: ")
-    if (response_4) == "Domain Name System" or "domain name system":
+    response_4 = input("Enter: ").upper()
+    if (response_4) == "Domain Name System":
         print("Correct!")
     else:
         print("Incorrect!")
@@ -53,8 +53,8 @@ def quiz():
 
     question_5 = "5) What does HTTP stands for?"
     print(question_5)
-    response_5 = input("Enter: ")
-    if (response_5) == "Hypertext Transfer Protocol" or "hypertext transfer protocol":
+    response_5 = input("Enter: ").upper()
+    if (response_5) == "Hypertext Transfer Protocol":
         print("Correct!")
     else:
         print("Incorrect!")
@@ -63,8 +63,8 @@ def quiz():
 
     question_6 = "6) What does OSI stands for?"
     print(question_6)
-    response_6 = input("Enter: ")
-    if (response_6) == "Open System Protocol" or "open system protocol":
+    response_6 = input("Enter: ").upper()
+    if (response_6) == "Open System Protocol":
         print("Correct!")
     else:
         print("Incorrect!")
@@ -73,8 +73,8 @@ def quiz():
 
     question_7 = "7) What does VPN stands for?"
     print(question_7)
-    response_7 = input("Enter: ")
-    if (response_7) == "Virtual Private Network" or "virtual private network":
+    response_7 = input("Enter: ").upper()
+    if (response_7) == "Virtual Private Network":
         print("Correct!")
     else:
         print("Incorrect!")
@@ -83,8 +83,8 @@ def quiz():
 
     question_8 = "8) What does DHCP stand for?"
     print(question_8)
-    response_8 = input("Enter: ")
-    if (response_8) == "Dynamic Host Configuration Protocol" or "dynamic host configuration protocol":
+    response_8 = input("Enter: ").upper()
+    if (response_8) == "Dynamic Host Configuration Protocol":
         print("Correct!")
     else:
         print("Incorrect!")
@@ -93,8 +93,8 @@ def quiz():
 
     question_9 = "9) What does LAN stand for?"
     print(question_9)
-    response_9 = input("Enter: ")
-    if response_9 == "Local Area Network" or "local area network":
+    response_9 = input("Enter: ").upper()
+    if response_9 == "Local Area Network":
         print("Correct!")
     else:
         print("Incorrect!")
@@ -103,8 +103,8 @@ def quiz():
 
     question_10 = "10) What does FTP stand for?"
     print(question_10)
-    response_10 = input("Enter: ")
-    if (response_10) == "File Transfer Protocol" or "file transfer protocol":
+    response_10 = input("Enter: ").upper()
+    if (response_10) == "File Transfer Protocol":
         print("Correct")
     else:
         print("Incorrect!")
